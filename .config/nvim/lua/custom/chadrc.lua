d4e8dd2462ddce45d8b17ac518dc4baac07fe154
Marskey <marskeymill@icloud.com>
2023-03-23 16:34:42 +0800

    init my first dotfiles

---
+++ .config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,24 @@
+-- First read our docs (completely) then check the example_config repo
+
+local M = {}
+
+M.mappings = require "custom.mappings"
+
+M.plugins = "custom.plugins"
+
+M.ui = {
+  theme = "everforest",
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "block",
+    overriden_modules = nil,
+  },
+  transparency = true,
+  tabufline = {
+    enabled = false,
+  },
+}
+
+return M
---
+++ .config/nvim/lua/custom/plugins_conf/overrides.lua
@@ -0,0 +1,55 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "c",
+    "markdown",
+    "markdown_inline",
+  },
+  additional_vim_regex_highlighting = false,
+  indent = {
+    enable = true,
+    disable = {
+      "python"
+    },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+M.cmp = {
+    completion = {
+        completeopt = "menu,menuone,noselect",
+    },
+}
+
+M.colorizer = {
+    user_default_options = {
+        names = false
+    }
+}
+return M
