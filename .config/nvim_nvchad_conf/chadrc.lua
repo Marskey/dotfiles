86ad97c618d29556e0b5c764b56a778fef5f8819
Marskey <marskeymill@icloud.com>
2023-08-26 18:03:51 +0800

    update

--- .config/nvim_nvchad_conf/chadrc.lua
+++ .config/nvim_nvchad_conf/chadrc.lua
@@ -7,19 +7,19 @@ M.mappings = require("custom.mappings")
 M.plugins = "custom.plugins"
 
 M.ui = {
-	theme = "everforest",
-	statusline = {
-		theme = "default", -- default/vscode/vscode_colored/minimal
-		-- default/round/block/arrow separators work only for default statusline theme
-		-- round and block will work for minimal theme only
-		separator_style = "round",
-		overriden_modules = nil,
-	},
+    theme = "everforest",
+    statusline = {
+        theme = "default", -- default/vscode/vscode_colored/minimal
+        -- default/round/block/arrow separators work only for default statusline theme
+        -- round and block will work for minimal theme only
+        separator_style = "block",
+        overriden_modules = nil,
+    },
 
-	transparency = true,
-	tabufline = {
-		enabled = false,
-	},
+    transparency = true,
+    tabufline = {
+        enabled = false,
+    },
 
     hl_override = {
         -- CursorLine = { bg = "darker_black" }
