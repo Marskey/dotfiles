2d33bdfdfba6b33023a819f83b3b405386738df4
Marskey <marskeymill@icloud.com>
2024-05-29 10:13:38 +0800

    update

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -70,6 +70,34 @@ return {
         fzf_colors = {
           ["gutter"] = "-1",
         },
+        keymap = {
+          builtin = {
+            ["<F1>"] = "toggle-help",
+            ["<c-n>"] = "toggle-fullscreen",
+            -- Only valid with the 'builtin' previewer
+            ["<F3>"] = "toggle-preview-wrap",
+            ["<c-p>"] = "toggle-preview",
+            ["<F5>"] = "toggle-preview-ccw",
+            ["<F6>"] = "toggle-preview-cw",
+            ["<C-d>"] = "preview-page-down",
+            ["<C-u>"] = "preview-page-up",
+            ["<S-left>"] = "preview-page-reset",
+          },
+          fzf = {
+            ["ctrl-z"] = "abort",
+            ["ctrl-f"] = "half-page-down",
+            ["ctrl-b"] = "half-page-up",
+            ["ctrl-a"] = "beginning-of-line",
+            ["ctrl-e"] = "end-of-line",
+            ["alt-a"] = "toggle-all",
+            -- Only valid with fzf previewers (bat/cat/git/etc)
+            ["f3"] = "toggle-preview-wrap",
+            ["f4"] = "toggle-preview",
+            ["ctrl-d"] = "preview-page-down",
+            ["ctrl-u"] = "preview-page-up",
+            ["ctrl-q"] = "select-all+accept",
+          },
+        },
       }
     end,
     init = function()
