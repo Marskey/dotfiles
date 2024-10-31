6d2538a78eb74e75c8ab0da4589c1b8a4c1e434c
Marskey <marskeymill@icloud.com>
2024-10-31 17:40:48 +0800

    update chadrc for nvchadui update

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -1,7 +1,6 @@
 -- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
----@type ChadrcConfig
 local M = {}
 
 M.ui = {
@@ -19,6 +18,15 @@ M.ui = {
   },
 
   -- transparency = true,
+}
+
+M.base46 = {
+  hl_override = {
+    CursorLine = { bg = "black2" },
+    -- 	Comment = { italic = true },
+    -- 	["@comment"] = { italic = true },
+  },
+
   hl_add = {
     -- BufferLineIndicatorSelected = { link = "BufferLineSeparator" }
     FlashLabel = { fg = "black", bg = "red" },
@@ -27,11 +35,6 @@ M.ui = {
     DiagnosticUnderlineWarn = { undercurl = true, sp = "yellow" },
     -- TabLine = { link = "NonText" }, -- xxx cterm=underline ctermfg=15 ctermbg=242 gui=underline guibg=DarkGrey
     -- TabLineFill = { link = "WinBar" }, --    xxx cterm=reverse gui=reverse
-  },
-  hl_override = {
-    CursorLine = { bg = "black2" },
-    -- 	Comment = { italic = true },
-    -- 	["@comment"] = { italic = true },
   },
 }
 return M
