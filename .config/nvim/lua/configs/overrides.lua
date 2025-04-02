cef7dfdf31af6da3b48a0119b791d02a74bc3e00
Marskey <marskeymill@icloud.com>
2025-04-02 09:52:16 +0800

    update

--- .config/nvim/lua/configs/overrides.lua
+++ .config/nvim/lua/configs/overrides.lua
@@ -221,15 +221,15 @@ M.cmp = {
 
 M.codecompanion = {
   adapters = {
-    deepseek = function ()
+    deepseek = function()
       return require("codecompanion.adapters").extend("deepseek", {
         schema = {
           model = {
-            default = "deepseek-chat"
-          }
-        }
+            default = "deepseek-chat",
+          },
+        },
       })
-    end
+    end,
   },
   strategies = {
     -- Change the default chat adapter
@@ -275,6 +275,62 @@ M.codecompanion = {
   -- opts = {
   --   log_level = "DEBUG", -- or "TRACE"
   -- },
+}
+
+M.blinkcmp = {
+  -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+  -- 'super-tab' for mappings similar to vscode (tab to accept)
+  -- 'enter' for enter to accept
+  -- 'none' for no mappings
+  --
+  -- All presets have the following mappings:
+  -- C-space: Open menu or open docs if already open
+  -- C-n/C-p or Up/Down: Select next/previous item
+  -- C-e: Hide menu
+  -- C-k: Toggle signature help (if signature.enabled = true)
+  --
+  -- See :h blink-cmp-config-keymap for defining your own keymap
+  keymap = {
+    preset = "enter",
+    ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+    ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+    ["<C-space>"] = {},
+    ["<C-k>"] = {
+      "show",
+      "show_documentation",
+      "hide_documentation",
+    },
+  },
+
+  appearance = {
+    -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+    -- Adjusts spacing to ensure icons are aligned
+    nerd_font_variant = "mono",
+  },
+
+  -- (Default) Only show the documentation popup when manually triggered
+  completion = { documentation = { auto_show = false } },
+
+  -- Default list of enabled providers defined so that you can extend it
+  -- elsewhere in your config, without redefining it, due to `opts_extend`
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer", "copilot" },
+    providers = {
+      copilot = {
+        name = "copilot",
+        module = "blink-cmp-copilot",
+        score_offset = 100,
+        async = true,
+      },
+    },
+  },
+
+  -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+  -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+  -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+  --
+  -- See the fuzzy documentation for more information
+  fuzzy = { implementation = "prefer_rust_with_warning" },
 }
 
 return M
