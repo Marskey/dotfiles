c67383ba576f360897d988681ce09a88f5d30545
Marskey <marskeymill@icloud.com>
2025-05-07 11:59:08 +0800

    update

--- .config/nvim/lua/configs/overrides.lua
+++ .config/nvim/lua/configs/overrides.lua
@@ -211,6 +211,18 @@ M.flash = {
 -- }
 
 M.codecompanion = {
+  display = {
+    action_palette = {
+      width = 95,
+      height = 10,
+      prompt = "Prompt ", -- Prompt used for interactive LLM calls
+      provider = "default", -- Can be "default", "telescope", "mini_pick" or "snacks". If not specified, the plugin will autodetect installed providers.
+      opts = {
+        show_default_actions = true, -- Show the default actions in the action palette?
+        show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+      },
+    },
+  },
   adapters = {
     deepseek = function()
       return require("codecompanion.adapters").extend("deepseek", {
@@ -301,6 +313,7 @@ M.blinkcmp = {
 
   -- (Default) Only show the documentation popup when manually triggered
   completion = {
+    keyword = { range = 'full' },
     documentation = { auto_show = false },
     list = {
       selection = {
