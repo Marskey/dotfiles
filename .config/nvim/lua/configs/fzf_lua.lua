f8a93a2e327bff2cb24cbf7c40cfeda70d53e927
Marskey <marskeymill@icloud.com>
2026-06-25 10:25:30 +0800

    feat(fzf-lua): support .fzf-lua-ignore in searches

--- .config/nvim/lua/configs/fzf_lua.lua
+++ .config/nvim/lua/configs/fzf_lua.lua
@@ -1,5 +1,36 @@
-local actions = require("fzf-lua").actions
-local utils = require("fzf-lua").utils
+local fzf_lua = require "fzf-lua"
+local actions = fzf_lua.actions
+local utils = fzf_lua.utils
+
+local fzf_lua_ignore_opts = "--ignore-file .fzf-lua-ignore"
+
+local function with_ignore_file(opts)
+  return opts .. " " .. fzf_lua_ignore_opts
+end
+
+local function with_rg_ignore_file(opts)
+  local opts_without_pattern_flag, count = opts:gsub("%s%-e%s*$", "")
+
+  if count > 0 then
+    return opts_without_pattern_flag .. " " .. fzf_lua_ignore_opts .. " -e"
+  end
+
+  return with_ignore_file(opts)
+end
+
+local function with_follow_symlinks(opts)
+  local replaced, count = opts:gsub("%s%-%-smart%-case", " -L --smart-case", 1)
+
+  if count > 0 then
+    return replaced
+  end
+
+  return opts .. " -L"
+end
+
+local function without_smart_case(opts)
+  return (opts:gsub("%s%-%-smart%-case", "", 1))
+end
 
 local function yank_filename(selected, opts)
   local ret = selected[1]:match "([%w-_]+%.%w+)"
@@ -31,7 +62,7 @@ local function hl_validate(hl)
   return not utils.is_hl_cleared(hl) and hl or nil
 end
 
-require("fzf-lua").setup {
+fzf_lua.setup {
   defaults = { formatter = "path.filename_first" },
   winopts = {
     height = 0.90,
@@ -123,6 +154,8 @@ require("fzf-lua").setup {
   },
 
   files = {
+    fd_opts = with_ignore_file(fzf_lua.defaults.files.fd_opts),
+    rg_opts = with_ignore_file(fzf_lua.defaults.files.rg_opts),
     fzf_opts = {
       ["--history"] = vim.fn.stdpath("data") .. "/fzf-lua-files-history",
     },
@@ -136,7 +169,7 @@ require("fzf-lua").setup {
   },
 
   grep = {
-    rg_opts = "--column --line-number --no-heading --color=always -L --smart-case --max-columns=4096 -e",
+    rg_opts = with_rg_ignore_file(with_follow_symlinks(fzf_lua.defaults.grep.rg_opts)),
     rg_glob = true,
     rg_glob_fn = function(query, opts)
       local regex, flags = query:match("^(.-)%s%-%-(.*)$")
@@ -156,17 +189,17 @@ require("fzf-lua").setup {
   },
 
   lgrep_quickfix = {
-      rg_opts = "--column --line-number --no-heading --color=always -L --max-columns=4096 -e",
-      rg_glob = true,
-      rg_glob_fn = function(query, opts)
-        local regex, flags = query:match("^(.-)%s%-%-(.*)$")
-        return (regex or query), flags
-      end,
-      ["ctrl-s"] = actions.file_split,
-      ["ctrl-y"] = yank_filename,
-      ["alt-i"] = { fn = actions.toggle_ignore, reuse = true, header = false },
-      ["alt-h"] = { fn = actions.toggle_hidden, reuse = true, header = false },
-      ["alt-f"] = { fn = actions.toggle_follow, reuse = true, header = false },
+    rg_opts = with_rg_ignore_file(without_smart_case(with_follow_symlinks(fzf_lua.defaults.grep.rg_opts))),
+    rg_glob = true,
+    rg_glob_fn = function(query, opts)
+      local regex, flags = query:match("^(.-)%s%-%-(.*)$")
+      return (regex or query), flags
+    end,
+    ["ctrl-s"] = actions.file_split,
+    ["ctrl-y"] = yank_filename,
+    ["alt-i"] = { fn = actions.toggle_ignore, reuse = true, header = false },
+    ["alt-h"] = { fn = actions.toggle_hidden, reuse = true, header = false },
+    ["alt-f"] = { fn = actions.toggle_follow, reuse = true, header = false },
   },
 
   oldfiles = {
